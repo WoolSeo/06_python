9a6721bfd5123a8c438230d8167b0a4ebca2936a
Wool Seo <wool@wool.pe.kr>
2017-09-06 09:02:23 +0900

    1a

--- paper/1a_numpy_170902_t_x.py
+++ paper/1a_numpy_170902_t_x.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 #import numpy as np
 
 N = 100
-t_max =100 
-p = 0.3
+t_max =10000
+p = 0.9
 q = 1-p
 cnt = 0
 multi = 1
@@ -29,7 +29,7 @@ var = np.array([])
 def randwalk(t):
     
     store_x = np.arange(0,t_max,1,dtype=np.int)
-    store_t = np.arange(t_max,dtype=np.int)
+
     #N번 시행
     for j in range(1, N):
         x = np.array([],dtype=np.int)
@@ -47,41 +47,39 @@ def randwalk(t):
       
         #t>1
         for i in range(1, t_max):
-            if( np.random.random() < q):
+            if( np.random.random() < p):
+                s_next = s[np.random.randint(0,i)]
+            else:
                 r = np.random.random()
                 if( r < 0.5 ):
                     s_next = +1
                 else:
                     s_next = -1
-                
-            else:
-                s_next = s[np.random.randint(0,i)]
             
             x = np.insert(x,i,x[i-1]+s_next)
             s = np.insert(s,i,s_next)
         
         
         store_x = np.vstack((store_x,x))
-        print(store_x)
-        print("s")
-    
-    print("done1")
-    var = np.var(store_x,1)
-    print(var)
+        #print(store_x)
+        #print("done1")
     
     
-    print()
+    #var = np.var(store_x,1)
+    #print(var)
+    
     
     for i in range(1,N):
         plt.plot(store_x[0,:],store_x[i,:],'o')
         
         
-    #plt.axis([10, t_max, 10, t_max*t_max])
+    plt.axis([0, t_max, -t_max, t_max])
     #plt.xscale('log')  
     #plt.yscale('log')  
     plt.xlabel('$t$', fontsize=15)
-    plt.ylabel('$< {x_t}^2> - {< x_t >}^2$', fontsize=15)
-    #plt.title(title)
+    plt.ylabel('$x(t)$', fontsize=15)
+
+    plt.title("$x(t)$, $p=$" + str(p)  )
 
     plt.show()
                 
