a3f5658fa0d2b7d1f945187f2b374c842abcfbca
Wool Seo <wool@wool.pe.kr>
2017-09-15 22:01:13 +0900

    re

--- random_test.py
+++ random_test.py
@@ -7,36 +7,20 @@ Created on Fri Sep 15 21:12:40 2017
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import multiprocessing as proc
 import time
-import datetime
 
-N = 1000
-t_max =10
-a = 0
-b = 0
+N = 100
 
-def randwalk(b,q):    
-    thistime = int(time.time()*10000000)
-    random.seed(thistime)
-    if(random.random()<0.5):
-        a = a + 1
-    else:
-        b = b + 1
-        
-    print(a)
-    print(b)
+def test(b,q):    
+    print(np.random.random())
+    
 
 
-
-
-startTime = time.time()
+######### 
 
 Q = proc.Queue() # queue
-
 p = [] #proceesing array
-
 for i in range(N):
-    p.append( proc.Process(target = randwalk, args=(i,Q)) )
+    p.append( proc.Process(target = test, args=(i,Q)) )
     p[i].start()
